6692081c2b721804d805ee3287f251e21524f21e
Sarah <dousse.sarah@gmail.com>
2022-01-06 11:52:00 +0000

    #18 pass first jest test

--- src/user.js
+++ src/user.js
@@ -1,2 +1,7 @@
-module.exports = User;
+class User {
 
+}
+
+module.exports = User;
+// always at the end or we get error : ReferenceError: Cannot access 'User' before initialization
+// classes must be defined before they can be constructed
